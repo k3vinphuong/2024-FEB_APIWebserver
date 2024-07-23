7205be2242164a699f303b87dd0bada7215d0ac5
k3vinphuong <kphuong230900@gmail.com>
2024-07-23 12:53:31 +1000

    first commit

---
+++ init.py
@@ -0,0 +1,5 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask import Flask
+from flask_bcrypt import bcrypt
+from flask_jwt_extended import JWTManager
+from flask_marshmallow import Marshmallow
