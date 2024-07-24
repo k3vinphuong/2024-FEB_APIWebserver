7315f8ffbae16fee0da1db682216635ae3767736
k3vinphuong <kphuong230900@gmail.com>
2024-07-24 15:29:00 +1000

    added card model, related user and meals

--- models/user.py
+++ models/user.py
@@ -1,23 +1,25 @@
 from init import db, ma
+from marshmallow import fields
 
 class User(db.Model):
     __tablename__ = "users"
 
-    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String)
     email = db.Column(db.String, nullable=False, unique=True)
     password = db.Column(db.String, nullable = False)
     date_of_birth = db.Column(db.Date)
     gender = db.Column(db.String)
     height = db.Column(db.Float, nullable = False)
-    weight = db.Cloumn(db.Float, nullable = False)
+    weight = db.Column(db.Float, nullable = False)
     
-class UserSchema(ma.Schema):
-    class Meta:
-        fields = ("id", "username", "email", "password", "dateofbirth", "gender", "height", "weight", "is_admin")
+    meals = db.relationship('Meal', back_populates="user")
 
+class UserSchema(ma.Schema):
+    meals = fields.List(fields.Nested('MealSchema', exclude=["user"]))
+    class Meta:
+        fields = ( "user_id", "username", "email", "password", "dateofbirth", "gender", "height", "weight", "is_admin", "meals" )
 
 user_schema = UserSchema(exclude=["password"])
-
 users_schema = UserSchema(many=True, exclude=["password"])
 
